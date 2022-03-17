51b29680c85be2018e4003c81cc5f075bb981de1
Rafael <rafael@iswe.co.nz>
2022-03-18 12:04:40 +1300

    fix outputs

--- outputs.tf
+++ outputs.tf
@@ -8,14 +8,19 @@ output "default_domain" {
   value       = aws_amplify_app.this.default_domain
 }
 
-output "domain_association_arn" {
+output "master_domain_association_arn" {
   description = "The ARN of the domain association resource."
-  value       = var.domain_name == "" ? null : join("", concat([""], aws_amplify_domain_association.this.*.arn))
+  value       = var.master_domain_name == "" ? null : join("", concat([""], aws_amplify_domain_association.master.*.arn))
+}
+
+output "develop_domain_association_arn" {
+  description = "The ARN of the domain association resource."
+  value       = var.master_domain_name == "" ? null : join("", concat([""], aws_amplify_domain_association.develop.*.arn))
 }
 
 output "custom_domains" {
   description = "List of custom domains that are associated with this resource (if any)."
-  value = var.domain_name == "" ? [] : [
+  value = var.master_domain_name == "" ? [] : [
     var.domain_name,
     "${var.master_subdomain_prefix}.${var.master_domain_name}",
     "${var.develop_subdomain_prefix}.${var.develop_domain_name}",
@@ -23,21 +28,25 @@ output "custom_domains" {
 }
 
 output "master_webhook_arn" {
+  count       = var.master_environment_enabled ? 1 : 0
   description = "The ARN of the master webhook."
   value       = aws_amplify_webhook.master[count.index].arn
 }
 
 output "master_webhook_url" {
+  count       = var.master_environment_enabled ? 1 : 0
   description = "The URL of the master webhook."
   value       = aws_amplify_webhook.master[count.index].url
 }
 
 output "develop_webhook_arn" {
+  count       = var.develop_environment_enabled ? 1 : 0
   description = "The ARN of the develop webhook."
   value       = aws_amplify_webhook.develop[count.index].arn
 }
 
 output "develop_webhook_url" {
+  count       = var.develop_environment_enabled ? 1 : 0
   description = "The URL of the develop webhook."
   value       = aws_amplify_webhook.develop[count.index].url
 }
